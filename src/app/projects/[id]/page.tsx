5daac6332d543fb304d31ff0d9aa3b0aa8c7e574
Oleg Mitus <mitusos@melonfashion.com>
2025-09-24 15:58:00 +0300

    fixes

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,22 +1,24 @@
 import Project from "@/components/projects/Project";
 import { projectsBlocks } from "@/data/projects";
 
-export async function generateStaticParams(): Promise<{ id: number }[]> {
+export async function generateStaticParams(): Promise<{ id: string }[]> {
   const pages = projectsBlocks[0].projects;
 
   return pages.map((project) => ({
-    id: project.id,
+    id: project.id.toString(),
   }));
 }
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: number }>;
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
 
-  const project = projectsBlocks[0].projects.find((item) => item.id == id);
+  const project = projectsBlocks[0].projects.find(
+    (item) => item.id.toString() === id
+  );
 
   if (!project) return "";
 

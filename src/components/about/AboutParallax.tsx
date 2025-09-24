21c499f4aaeed45aec7a2ebd1047eca7b5a23c41
Oleg Mitus <mitusos@melonfashion.com>
2025-09-24 16:10:06 +0300

    Image path build

--- src/components/about/AboutParallax.tsx
+++ src/components/about/AboutParallax.tsx
@@ -18,31 +18,31 @@ const ProjectParallax: FC = () => {
         className="w-full absolute inset-0 z-[-1] -top-40"
         ref={line1}
       >
-        <Image src="/line-3_1.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-3_1.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </motion.div>
       <motion.div
         className="w-full absolute inset-0 z-[-1] -top-40"
         ref={line2}
       >
-        <Image src="/line-3_2.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-3_2.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </motion.div>
       <div
         className="w-full absolute inset-0 z-[-1] top-300"
         ref={line3}
       >
-        <Image src="/line-3_3.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-3_3.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </div>
       <div
         className="w-full absolute inset-0 z-[-1] top-300"
         ref={line4}
       >
-        <Image src="/line-3_4.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-3_4.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </div>
       <div
         className="w-full absolute inset-0 z-[-1] top-500"
         ref={line5}
       >
-        <Image src="/line-3_5.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-3_5.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </div>
     </motion.div>
   );
--- src/components/projects/ProjectsParallax.tsx
+++ src/components/projects/ProjectsParallax.tsx
@@ -17,25 +17,25 @@ const ProjectsParallax: FC = () => {
         className="w-full absolute inset-0 z-[-1] -top-20"
         ref={line1}
       >
-        <Image src="/line-1_1.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-1_1.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </motion.div>
       <motion.div
         className="w-full absolute inset-0 z-[-1] -top-40"
         ref={line2}
       >
-        <Image src="/line-1_2.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-1_2.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </motion.div>
       <div
         className="w-full absolute inset-0 z-[-1] top-280"
         ref={line3}
       >
-        <Image src="/line-1_3.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-1_3.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </div>
       <div
         className="w-full absolute inset-0 z-[-1] top-300"
         ref={line4}
       >
-        <Image src="/line-1_4.svg" width={1920} height={800} alt="" className="w-full h-auto" />
+        <Image src="line-1_4.svg" width={1920} height={800} alt="" className="w-full h-auto" />
       </div>
     </motion.div>
   );

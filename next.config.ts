2fbaaf716f39ec650ffb053c4dc755b00ea4482e
Oleg Mitus <mitusos@melonfashion.com>
2025-09-27 23:09:12 +0300

    Localization

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const config: NextConfig = {
   output: "export",
-  basePath: "/nextera",
-  // Other Next.js configuration options
+  experimental: {
+    turbo: {}, // Add this line
+  },
 };
 
 export default withNextIntl(config);
